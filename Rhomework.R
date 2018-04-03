54bbe8507a5866e989177f361d641833c026c36f
cbward5 <connor_ward@my.uri.edu>
2018-04-03 12:38:58 -0400

    Basic

---
+++ Rhomework.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+download.file("https://ndownloader.figshare.com/files/2292169",
+              "data/portal_data_joined.csv")
+surveys <- read_csv("data/portal_data_joined.csv")
+head(surveys)
