b52a69961f2b1771653724c6c6c990d4b5cb6e12
cbward5 <connor_ward@my.uri.edu>
2018-04-03 22:06:02 -0400

    Added two graphs

--- Rhomework.R
+++ Rhomework.R
@@ -3,3 +3,4 @@ download.file("https://ndownloader.figshare.com/files/2292169",
               "data/portal_data_joined.csv")
 surveys <- read_csv("data/portal_data_joined.csv")
 head(surveys)
+#seeing if this works
