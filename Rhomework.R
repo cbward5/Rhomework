f0f640fa92f93e6037c09b12146b9afa99129dbc
cbward5 <connor_ward@my.uri.edu>
2018-04-23 15:24:52 -0400

    r-squared and p-value added to comments

--- Rhomework.R
+++ Rhomework.R
@@ -10,25 +10,34 @@ surveys_complete <- surveys %>%
 
 weightplot <- ggplot(surveys_complete, aes(x=species_id, y = weight)) +
   geom_jitter(alpha = .1,color = "blue") + geom_boxplot(alpha =.5) +
-  xlab("Species ID")+ylab("Weight (kg)")
+  xlab("Species ID")+ylab("Weight (kg)") +  ggtitle("Individual Weight by Species")
 
 lengthplot <- ggplot(surveys_complete, aes(x=species_id, y = hindfoot_length)) +
   geom_jitter(alpha = .1,color = "red") + geom_boxplot(alpha =.5) +
-  xlab("Species ID")+
-  ylab("Hindfoot Length (cm)")
+  xlab("Species ID")+ylab("Hindfoot Length (cm)")+  ggtitle("Individual Hindfoot Length by Species")
+
 
 grid.arrange(weightplot,lengthplot)
 
-#need to do r-squared and p-values
 ggplot(surveys_complete, aes(x=weight, y = hindfoot_length)) +
   geom_point(alpha = .05,color = "blue") + xlab("Weight (kg)")+
-  ylab("Hindfoot Length (cm)")
+  ylab("Hindfoot Length (cm)")+ggtitle("Weight vs. Hindfoot Length of Individuals")
+
+
+weightlength.lm <- lm(weight ~ hindfoot_length, data=surveys_complete)
+#summary(weightlength.lm)
+#r-squared 0.4673
+#p 2.2e-16
 
 mean_wl <- surveys_complete %>% group_by(species_id) %>%
   summarize(mean_length = mean(hindfoot_length),
             mean_weight = mean(weight))
 
-#need to do r-squared and p-values
 ggplot(mean_wl, aes(x=mean_weight, y=mean_length, color=species_id))+
-  geom_point()+xlab("Mean Weight (kg)")+ylab("Mean Hindfoot Length (cm)")
+  geom_point()+xlab("Mean Weight (kg)")+ylab("Mean Hindfoot Length (cm)")+
+  ggtitle("Average Weight vs. Hindfoot Length by Species")
 
+mean.lm <- lm(mean_weight ~ mean_length, data=mean_wl)
+#summary(mean.lm)
+#R-squared 0.5784
+#p 9.728e-06
